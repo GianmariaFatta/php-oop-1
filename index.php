9d14bccfb3f5043e567e86328c415f7440b8c30b
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-02-17 16:45:48 +0100

    add construct

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php 
  
 class Movie{
-    
+
     public $title;
     public $genre;
     public $language;
     public $overview;
+
+    public function __construct($_title,$_genre,$_language,$_overview)
+    {
+        $this->title=$_title;
+        $this->genre=$_genre;
+        $this->language=$_language;
+        $this->$overview=$_overview;
+    }
 }
 
+$il_signore_degli_anelli = new Movie()
